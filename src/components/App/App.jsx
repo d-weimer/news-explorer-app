63bb0505728759207694d20adb4a8fb89823b87f
Daniel Weimer <dweimer19@gmail.com>
2026-07-11 23:58:34 +0900

    Added Routes for home and saved-news

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,40 @@
 import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import Header from "../Header/Header.jsx";
 import SearchForm from "../SearchForm/SearchForm.jsx";
 import About from "../About/About.jsx";
 import Footer from "../Footer/Footer.jsx";
+import SavedNews from "../SavedNews/SavedNews.jsx";
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute.jsx";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div className="page">
       <div className="page__content">
-        <Header />
-        <SearchForm />
-        <About />
+        <Header isLoggedIn={isLoggedIn} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <SearchForm />
+                <About />
+              </>
+            }
+          />
+          <Route
+            path="/saved-news"
+            element={
+              <ProtectedRoute isLoggedIn={isLoggedIn}>
+                <SavedNews />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
         <Footer />
       </div>
     </div>
---
+++ src/components/SavedNews/SavedNews.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./SavedNews.css";
+
+function SavedNews() {
+  return (
+    <div className="saved-news">
+      <h2 className="saved-news__title">Your Saved Articles</h2>
+      <p className="saved-news__subtitle">You have 0 saved articles.</p>
+    </div>
+  );
+}
+
+export default SavedNews;
